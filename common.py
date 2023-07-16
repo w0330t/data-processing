c478c8f1d3095b5401e60b1a42970d091509cf4b
Blueness <w0330t@gmail.com>
2023-07-16 12:18:05 +0800

    删除多余空格

--- common.py
+++ common.py
@@ -8,19 +8,19 @@ from pyecharts.globals import ThemeType
 def read_kline_data(file_path:str) -> pd.DataFrame:
     """
     从给定的 JSON 文件中读取K线数据，并重命名列名为指定的名称
-    
+
     Args:
         file_path(str):JSON 文件的路径
-        
+
     Returns:
         DataFrame:一个包含重命名后的列名的 DataFrame 对象
     """
     # 读取 JSON 文件数据
     df = pd.read_json(file_path)
-    
+
     # 重命名列名
     df = df.rename(columns={0: 'timestamp', 1: 'open', 2: 'high', 3: 'low', 4: 'close', 5: 'volume'})
-    
+
     # 添加自然时间列
     df['datetime'] = df['timestamp'].apply(convert_timestamp)
 
